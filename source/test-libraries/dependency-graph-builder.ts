58d8548ed20b8bfcf970f0bc7491b95ee6960ca9
Mathias Schreck <schreck.mathias@gmail.com>
2023-09-13 21:42:38 +0200

    Implement unit tests for substitute-bundles

---
+++ source/test-libraries/dependency-graph-builder.ts
@@ -0,0 +1,44 @@
+import {Maybe} from 'true-myth';
+import {Project} from 'ts-morph';
+import {DependencyGraph, createDependencyGraph} from '../dependency-scanner/dependency-graph.js';
+import {createProject} from './typescript-project.js';
+
+interface Entry {
+    filePath: string;
+    content: string;
+    topLevelDependencies?: Map<string, string>;
+    dependencies?: Entry[];
+}
+
+interface Options {
+    entries?: Entry[];
+}
+
+function addEntries(graph: DependencyGraph, project: Project, entries: Entry[], parentFilePath: string | null): void {
+    for (const entry of entries) {
+        const sourceFile = project.createSourceFile(entry.filePath, entry.content);
+        graph.addDependency(entry.filePath, {
+            sourceMapFilePath: Maybe.nothing(),
+            topLevelDependencies: entry.topLevelDependencies ?? new Map(),
+            substitutionContent: Maybe.nothing(),
+            tsSourceFile: sourceFile
+        });
+        if (parentFilePath !== null) {
+            graph.connect(parentFilePath, entry.filePath);
+        }
+
+        if (Array.isArray(entry.dependencies)) {
+            addEntries(graph, project, entry.dependencies, entry.filePath);
+        }
+    }
+}
+
+export function buildDependencyGraph(options: Options = {}): DependencyGraph {
+    const {entries = []} = options;
+    const graph = createDependencyGraph();
+    const project = createProject();
+
+    addEntries(graph, project, entries, null);
+
+    return graph;
+}
