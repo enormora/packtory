503ecbc510480848d651c27b16090457465bda5c
Mathias Schreck <schreck.mathias@gmail.com>
2023-09-21 20:54:57 +0200

    Update dependencies

--- source/test-libraries/tar.ts
+++ source/test-libraries/tar.ts
@@ -12,7 +12,6 @@ export async function extractTarEntries(buffer: Buffer): Promise<Entry[]> {
     const stream = Readable.from(buffer).pipe(createGunzip()).pipe(extractStream);
     const entries: Entry[] = [];
 
-    // @ts-expect-error
     for await (const entry of stream) {
         let result = '';
         for await (const chunk of entry) {
