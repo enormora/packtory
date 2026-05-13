a40f8fa8e833f66578a34546783c2e07154608b6
Mathias Schreck <schreck.mathias@gmail.com>
2026-05-13 13:28:54 +0200

    Upgrade diff and remove custom typing shim

--- source/common/typed-diff.ts
+++ source/common/typed-diff.ts
@@ -1,20 +1,4 @@
-import { createRequire } from 'node:module';
-
-type StructuredPatchHunk = {
-    readonly oldStart: number;
-    readonly oldLines: number;
-    readonly newStart: number;
-    readonly newLines: number;
-    readonly lines: readonly string[];
-};
-
-type StructuredPatchResult = {
-    readonly hunks: readonly StructuredPatchHunk[];
-};
-
-type StructuredPatchOptions = {
-    readonly context?: number;
-};
+import { structuredPatch, type StructuredPatch, type StructuredPatchOptionsNonabortable } from 'diff';
 
 type StructuredPatchArgs = readonly [
     oldFileName: string,
@@ -23,21 +7,9 @@ type StructuredPatchArgs = readonly [
     newStr: string,
     oldHeader?: string,
     newHeader?: string,
-    options?: StructuredPatchOptions
+    options?: StructuredPatchOptionsNonabortable
 ];
 
-type StructuredPatchFunction = (...args: StructuredPatchArgs) => StructuredPatchResult;
-
-type DiffModule = {
-    readonly structuredPatch: StructuredPatchFunction;
-};
-
-const require = createRequire(import.meta.url);
-/* eslint-disable-next-line import/no-commonjs, @typescript-eslint/no-unsafe-assignment -- the diff package ships untyped ESM, so this adapter constrains it once at the boundary */
-const untypedDiffModule = require('diff');
-/* eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion -- the local DiffModule type constrains the single imported surface we rely on */
-const diffModule = untypedDiffModule as DiffModule;
-
-export function createStructuredPatch(...args: StructuredPatchArgs): StructuredPatchResult {
-    return diffModule.structuredPatch(...args);
+export function createStructuredPatch(...args: StructuredPatchArgs): StructuredPatch {
+    return structuredPatch(...args);
 }
